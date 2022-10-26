043785df2a01e6fb0fb8274a1c6f3599a6b09f81
WQhuanm <2942096269@qq.com>
2022-10-26 16:11:32 +0800

    Add files via upload

---
+++ Sheikh (Hard Version).cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+const int INF = 0x3f3f3f3f;
+const int N = 2e5 + 100;
+
+void mymin()
+{
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        mymin();
+    }
+
+    system("pause");
+    return 0;
+}
